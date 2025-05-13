2fbb3c47981421e1dbd15e736bb41cebaf638524
ByteYue <yj976240184@gmail.com>
2025-05-13 15:20:54 +0800

    comment

--- rust/server-framework/src/lib.rs
+++ rust/server-framework/src/lib.rs
@@ -1,8 +1,8 @@
 // Copyright © Aptos Foundation
 
 use anyhow::{Context, Result};
-#[cfg(target_os = "linux")]
-use aptos_system_utils::profiling::start_cpu_profiling;
+// #[cfg(target_os = "linux")]
+// use aptos_system_utils::profiling::start_cpu_profiling;
 use backtrace::Backtrace;
 use clap::Parser;
 use prometheus::{Encoder, TextEncoder};
